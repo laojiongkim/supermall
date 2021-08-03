372bfc6c55f907e84cc68d70b5e235a25b6897e7
laojiongkim <124317824@qq.com>
2021-08-03 21:10:31 +0800

    主页面滚动图与推荐组件添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,42 @@
 <template>
-    <h2>首页</h2>
+    <div id="home">
+        <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+        <home-swiper :banners="banners" />
+        <recommend-view :recommends="recommends" />
+    </div>
+    
 </template>
 <script>
-    export default {
-        name: "Home"
+import NavBar from "../../components/common/navbar/NavBar";
+import HomeSwiper from './childComps/HomeSwiper'
+import { getHomeMultidata } from "../../network/home";
+import RecommendView from './childComps/RecommendView';
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
     }
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
