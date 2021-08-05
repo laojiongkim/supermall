ba188ead2d199abf8866ca31ac8db666f31aeeb3
laojiongkim <124317824@qq.com>
2021-08-05 21:13:58 +0800

    特性内容添加

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,109 @@
         <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
         <home-swiper :banners="banners" />
         <recommend-view :recommends="recommends" />
+        <feature-view></feature-view>
+        <ul>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+          <li>列表</li>
+        </ul>
     </div>
     
 </template>
@@ -11,13 +114,17 @@ import NavBar from "../../components/common/navbar/NavBar";
 import HomeSwiper from './childComps/HomeSwiper'
 import { getHomeMultidata } from "../../network/home";
 import RecommendView from './childComps/RecommendView';
+import FeatureView from './childComps/FeatureView'
+// import FeatureView from './childComps/FeatureView.vue';
 
 export default {
   name: "Home",
   components: {
     NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
+    // FeatureView
   },
   data() {
     return {
@@ -35,8 +142,17 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 44px; 
+}
+
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
 </style>
